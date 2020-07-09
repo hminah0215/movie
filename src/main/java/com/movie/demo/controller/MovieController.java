d48d14ac8ace4d72b257b2219e9d65f2a29ede47
hminah0215 <hyeonminah@gmail.com>
2020-07-09 13:35:55 +0900

    민아) 크롤링테스트, movievo 변경

---
+++ src/main/java/com/movie/demo/controller/MovieController.java
@@ -0,0 +1,67 @@
+package com.movie.demo.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.google.gson.Gson;
+import com.movie.demo.vo.MovieVo;
+
+@Controller
+public class MovieController {
+
+	@ResponseBody
+	@RequestMapping(value = "crawling.do", method = { RequestMethod.GET,RequestMethod.POST }, produces = "text/plain;charset=UTF-8")
+	public String crawling() {
+		Document doc;
+		String str = "";
+//		String title = ""; // 영화 제목
+//		String score = ""; // 영화 평점
+//		String advance_rate = ""; // 영화 예매율
+//		String genre = ""; // 영화 장르
+//		String running_time = ""; // 영화 상영시간
+//		String openDate = ""; // 영화 개봉일
+//		String director = ""; // 영화 감독
+//		String actors = ""; // 영화 출연진
+//		int finalCnt = 0; // 예매순 1~20위 까지 가져온다
+
+		try {
+			// jsoup으로 네이버 상영영화 정보를 가져온다.
+			doc = Jsoup.connect("https://movie.naver.com/movie/running/current.nhn").get();
+			
+			Elements title = doc.select("dt.tit > a");
+			Elements score = doc.select(".info_star > .star_t1 > .num");
+			Elements advance_rate = doc.select(".lst_dsc > .info_exp > dd >.star_t1 > .num");
+//			Elements advance_rate = doc.select(".star_t1 > .num");
+			
+			List<MovieVo> list = new ArrayList<MovieVo>();
+			
+			for(int i= 0; i < 10; i++) {
+				String tit = title.get(i).text();
+				String scores = score.get(i).text();
+				String rate = advance_rate.get(i).text();
+				
+				MovieVo test = new MovieVo(tit, scores, rate ,"","","","","","");
+				
+				list.add(test);
+			}
+			Gson gson = new Gson();
+			str = gson.toJson(list);
+			
+			
+		} catch (Exception e) {
+			System.out.println("크롤링 오류: " + e.getMessage());
+		}
+		
+		return str;
+	}
+}
--- src/main/java/com/movie/demo/vo/MovieVo.java
+++ src/main/java/com/movie/demo/vo/MovieVo.java
@@ -8,16 +8,15 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 public class MovieVo {
-	private int m_no;
-	private String m_title;
-	private String m_runtime;
-	private String m_ageLimit;
-	private String m_genre;
-	private String m_opendate;
-	private String m_director;
-	private String m_cast;
-	private String m_summary;
-	private String m_poster;
-	private String expert_rating;
-	private String visitor_rating;
+	//제목, 평점, 예매율, 장르, 러닝타임, 개봉일, 감독, 배우, 이미지 
+	
+	private String title;			// 영화 제목
+	private String score; 			// 영화 평점
+	private String advance_rate; 	// 영화 예매율
+	private String genre; 			// 영화 장르
+	private String running_time; 	// 영화 상영시간
+	private String openDate; 		// 영화 개봉일
+	private String director; 		// 영화 감독
+	private String actors; 			// 영화 출연진
+	private String main_poster;		// 영화 포스터
 }
