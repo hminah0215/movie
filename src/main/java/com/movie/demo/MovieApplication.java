0fe3953397164d8b651a9c34bb5d111bb59b9d24
hminah0215 <hyeonminah@gmail.com>
2020-07-10 19:56:06 +0900

    머지

--- src/main/java/com/movie/demo/MovieApplication.java
+++ src/main/java/com/movie/demo/MovieApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MovieApplication {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(MovieApplication.class, args);
 	}
